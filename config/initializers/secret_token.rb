accc4c8b05a5108936af8ac7b37c107889c7d65b
Noah Paessel <knowuh@gmail.com>
2012-09-27 15:40:06 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MsAuthoring::Application.config.secret_token = 'd6b1b8a6737ac442f8d4cb53bfb1f079ea91ace0599a2a3a3729c21cba3c32d5120a6e6a11d2c280cac9b86104d893ff9e809612978c962b9d6fb13d1fe84013'
