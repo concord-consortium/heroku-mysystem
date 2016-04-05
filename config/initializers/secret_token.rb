e433da77a357715b413a76a466a3f786d4d122e9
Noah Paessel <knowuh@gmail.com>
2016-04-05 14:39:13 -0400

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MsAuthoring::Application.config.secret_token = 'd23335abfb1f079eab3339716bc8ee91aae7578a2a3a3729c21cba3c3332d5520a6e6a11d2c4444966fb13d1f88401123ecd917342c011ea8ccd739cdddc8763'
