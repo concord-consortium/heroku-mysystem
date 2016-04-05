c2424f37cd360767304b13737c269fbddf09f61b
Noah Paessel <knowuh@gmail.com>
2016-04-05 14:37:06 -0400

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MsAuthoring::Application.config.secret_token = 'd6b1b8a6737ac442f8d4cb53bfb1f079ea91ace0599a2a3a3729c21cba3c32d5120a6e6a11d2c280cac9b86104d893ff9e809612978c962b9d6fb13d1fe84013'
+MsAuthoring::Application.config.secret_token = 'd23335abfb1f079eab3339716bc8ee91aae7578a2a3a3729c21cba3c3332d5520a6e6a11d2c4444966fb13d1f88401123ecd917342c011ea8ccd739cdddc8763'
